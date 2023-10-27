76632a337de9927b3a1b1d90a035c49ba070f5b3
May Pyone <maypyone015@gmail.com>
2023-10-27 16:39:08 +0630

    Add implementations for methods

--- solver.rb
+++ solver.rb
@@ -1,6 +1,25 @@
 class Solver
   def factorial(num)
-    raise ArgumentError, "Negative numbers are not allowed." if num.negative?
-    return 24
+    raise ArgumentError, 'Negative numbers are not allowed.' if num.negative?
+    return 1 if [0, 1].include?(num)
+
+
+    num * factorial(num - 1)
+  end
+
+  def reverse(string)
+    string.reverse
+  end
+
+  def fizzbuzz(num)
+    if (num % 15).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
   end
 end
