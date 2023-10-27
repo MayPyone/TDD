dfc5d9746cb2231ddaedb13fe65e40a31f6053d8
binyamolango <olangobinyam@gmail.com>
2023-10-27 11:55:05 +0300

    Add test for the reverse method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,7 +4,6 @@ describe Solver do
   solver = Solver.new
 
   describe "#factorial" do
-
     it "should raise an exception if a number is negative" do
       expect { solver.factorial(-4) }.to raise_error(ArgumentError, "Negative numbers are not allowed.")
     end
@@ -16,6 +15,11 @@ describe Solver do
     it "should return the factorial of a number >= 0" do
       expect(solver.factorial(4)).to eq 24
     end
+  end
 
+  describe "#reverse" do
+    it "should return a reversed string" do
+      expect(solver.reverse("hello")).to eq "olleh"
+    end
   end
 end
