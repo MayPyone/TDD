347734a24122218bd2e4669a0f6fd7f933654930
May Pyone <maypyone015@gmail.com>
2023-10-27 16:27:20 +0630

    Implement the fizzbuzz

--- solver.rb
+++ solver.rb
@@ -10,4 +10,16 @@ class Solver
   def reverse(string)
     string.reverse
   end
+
+  def fizzbuzz(num)
+    if (num % 15).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
+    end
+  end
 end
