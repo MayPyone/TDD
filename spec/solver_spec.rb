d091786254a8414e0e92f9fdfaeeec7609e28b6b
binyamolango <olangobinyam@gmail.com>
2023-10-27 11:23:53 +0300

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,6 +1,13 @@
-require 'rspec'
-require_relative './solver'
+require './solver'
 
-RSpec.describe Solver do
+describe Solver do
+
+solver = Solver.new
   
+  describe "#factorial" do
+    it "should return the factorial of a number >= 0" do
+      expect(solver.factorial(4)).to eq 24
+    end
+  end
+
 end
