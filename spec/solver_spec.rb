28c7384aea7181d300104e3d3dad1bb978ce86fb
binyamolango <olangobinyam@gmail.com>
2023-10-27 11:52:06 +0300

    Add for rasing exception test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,13 +1,21 @@
 require './solver'
 
 describe Solver do
+  solver = Solver.new
 
-solver = Solver.new
-  
   describe "#factorial" do
+
+    it "should raise an exception if a number is negative" do
+      expect { solver.factorial(-4) }.to raise_error(ArgumentError, "Negative numbers are not allowed.")
+    end
+
+    it "does not raise an error for positive numbers" do
+      expect { solver.factorial(10) }.not_to raise_error
+    end
+
     it "should return the factorial of a number >= 0" do
       expect(solver.factorial(4)).to eq 24
     end
-  end
 
+  end
 end
