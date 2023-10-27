b705e5e3e470d45311427a7285500f94b185d4e5
May Pyone <maypyone015@gmail.com>
2023-10-27 16:15:37 +0630

    Implement the factorial method

--- solver.rb
+++ solver.rb
@@ -1,6 +1,9 @@
 class Solver
   def factorial(num)
-    raise ArgumentError, "Negative numbers are not allowed." if num.negative?
-    return 24
+    raise ArgumentError, 'Negative numbers are not allowed.' if num.negative?
+    return 1 if [0, 1].include?(num)
+
+
+    num * factorial(num - 1)
   end
 end
